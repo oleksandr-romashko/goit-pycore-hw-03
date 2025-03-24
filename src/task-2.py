4320249e0b0afb590161fa14e91f332a6ec587ca
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-03-24 02:33:44 +0100

    Fix task-2 validation and examples acc to the task requirements

--- src/task-2.py
+++ src/task-2.py
@@ -10,13 +10,13 @@ def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int]:
     '''
     # Validate input values guard
     if min < 1 or min > max:
-        raise ValueError("'min' value should be between 1 and 'max'")
+        return []
 
     if max > 1000 or max < min:
-        raise ValueError("'min' value should be between 1 and 'max'")
+        return []
 
     if quantity > (max - min + 1):
-        raise ValueError("'quantity' value should be between 'min' and 'max' values")
+        return []
 
     # Generate ticket numbers
     numbers = random.sample(range(min, max + 1), quantity)
@@ -46,13 +46,16 @@ test_numbers_min_numbers_quantity = get_numbers_ticket(400, 400, 1)
 assert len(test_numbers_min_numbers_quantity) == 1
 assert test_numbers_min_numbers_quantity == [400]
 
-test_numbers_sort_order = list(get_numbers_ticket(70, 150, 20))
-for i in range(1, len(test_numbers_sort_order)):
-    assert test_numbers_sort_order[i - 1] < test_numbers_sort_order[i]
+test_numbers_sort_order_unique_numbers = list(get_numbers_ticket(70, 150, 20))
+for i in range(1, len(test_numbers_sort_order_unique_numbers)):
+    assert test_numbers_sort_order_unique_numbers[i - 1] < test_numbers_sort_order_unique_numbers[i]
 
 # Usage
-min_number = 100
-max_number = 1000
-numbers_quantity = 5
+min_number = 1
+max_number = 49
+numbers_quantity = 6
+
+lottery_numbers = get_numbers_ticket(min_number, max_number, numbers_quantity)
+
 print(f"Lottery: min number value: {min_number}, max number value: {max_number}, number of tickets: {numbers_quantity}")
-print(f"Resulting lottery numbers: {get_numbers_ticket(min_number, max_number, numbers_quantity)}")
+print(f"Your lottery numbers: {lottery_numbers}")
